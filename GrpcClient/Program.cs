a4360342663470356915ea1c960efa209c75a5ad
Ezekiel Abodesegun <eabodesegun@appzonegroup.com>
2024-01-04 13:05:01 +0100

    unary implementation

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Grpc.Net.Client;
+using GrpcService.Protos;
+
+var channel = GrpcChannel.ForAddress("http://localhost:5281");
+var client = new Sample.SampleClient(channel);
+//var response = client.GetFullName(new SampleRequest { FirstName = "John", LastName = "Thomas" });
+var response = await client.GetFullNameAsync(new SampleRequest { FirstName = "John", LastName = "Thomas" });
+Console.WriteLine(response.FullName);
+Console.ReadLine();
+
+
---
+++ GrpcService/Services/SampleService.cs
@@ -0,0 +1,15 @@
+﻿using Grpc.Core;
+using GrpcService.Protos;
+using System.Linq.Expressions;
+
+namespace GrpcService.Services
+{
+    public class SampleService : Sample.SampleBase
+    {
+        public override  Task<SampleResponse> GetFullName(SampleRequest request, ServerCallContext context)
+        {
+            var result = $"{request.FirstName}  {request.LastName}";
+            return Task.FromResult(new SampleResponse { FullName = result });
+        }
+    }
+}
